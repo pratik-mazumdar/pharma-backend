a16c75348937f9f7c7bc739cae7bae767aa0c59f
Pratik Mazumdar <pratikmazumdar680@protonmail.com>
2025-03-07 18:39:54 +0530

    fix: remove cors

--- config/cors.ts
+++ config/cors.ts
@@ -7,7 +7,7 @@ import { defineConfig } from '@adonisjs/cors'
  * https://docs.adonisjs.com/guides/security/cors
  */
 const corsConfig = defineConfig({
-  enabled: true,
+  enabled: false,
   origin: true,
   methods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE'],
   headers: true,
