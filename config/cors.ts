3e07b682d91d30b07f4d1a4b7187f94e62323c73
Pratik Mazumdar <pratikmazumdar680@protonmail.com>
2025-03-07 19:08:00 +0530

    fix: remove cors

--- config/cors.ts
+++ config/cors.ts
@@ -7,13 +7,13 @@ import { defineConfig } from '@adonisjs/cors'
  * https://docs.adonisjs.com/guides/security/cors
  */
 const corsConfig = defineConfig({
-  enabled: false,
-  // origin: true,
-  // methods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE'],
-  // headers: true,
-  // exposeHeaders: [],
-  // credentials: true,
-  // maxAge: 90,
+  enabled: true,
+  origin: ['localhost', 'pharma.dreammonks.com'],
+  methods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE'],
+  headers: true,
+  exposeHeaders: [],
+  credentials: true,
+  maxAge: 90,
 })
 
 export default corsConfig
